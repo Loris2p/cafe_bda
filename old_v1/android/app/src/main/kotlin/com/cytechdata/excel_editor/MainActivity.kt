188982ef3b701e26e913a38af583a84db51675b4
Loris L <loris.lahon@gmail.com>
2026-03-15 10:59:24 +0000

    chore: nettoyage du dépôt et archivage de la v1

--- old_v1/android/app/src/main/kotlin/com/cytechdata/excel_editor/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.cytechdata.cafe_bda
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- old_v1/android/app/src/main/kotlin/com/example/excel_editor/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.cytechdata.exceleditor
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
